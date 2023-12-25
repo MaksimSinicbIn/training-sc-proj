0e49ecd3a4aeb9a5d941a574c5ccd2fb4d888b47
MaksimSinicbIn <dzigen1986@gmail.com>
2023-12-25 12:40:18 +0700

    Add all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Card } from './components/Card.styled';
+import { BodySpace } from './components/BodySpace.styled';
+import { Content } from './components/Content.styled';
+import { Headline } from './components/Headline.styled';
+import { Text } from './components/Text.styled';
+import { ButtonSpace, StyledBtn } from './components/Button.styled';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        <Content />
+        <BodySpace>
+          <Headline>Headline</Headline>
+          <Text>Faucibus. Faucibus. Sit sit sapien sit <br /> tempusrisu ut. Sit molestie ornare in venen.</Text>
+          <ButtonSpace>
+            <StyledBtn>See more</StyledBtn>
+            <StyledBtn>Save</StyledBtn>
+          </ButtonSpace>
+        </BodySpace>
+      </Card>
     </div>
   );
 }
