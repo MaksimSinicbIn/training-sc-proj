cadada4b39dbb0a8b26c9ac15ba0170f09c2fb42
MaksimSinicbIn <dzigen1986@gmail.com>
2023-12-26 17:54:17 +0700

    Add hover (scale and transition)

--- src/components/Content.styled.tsx
+++ src/components/Content.styled.tsx
@@ -8,4 +8,10 @@ export const Content = styled.div`
     background: url(${img}), lightgray 50% / cover no-repeat;
     background-size: cover;
     background-position: center;
+    transition: 0.7s;
+    
+    &:hover {
+    transform: scale(1.5);
+    transition: 0.7s
+    }
 `
