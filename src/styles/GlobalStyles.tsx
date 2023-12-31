19c55a6ab4daaedff6c313de6edf87114f342178
MaksimSinicbIn <dzigen1986@gmail.com>
2024-01-01 00:49:50 +0700

    Correct GlobalStyles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -13,6 +13,7 @@ export const GlobalStyles = createGlobalStyle`
     body{
     width: 100%;
     height: 100%;
+    font-family: 'Inter', sans-serif;
     display: flex;
     align-items: center;
     justify-content: center;
